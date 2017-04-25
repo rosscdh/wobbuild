a3ba7ef39e7e3a79ecb101c2bfa99ef1c7b73ee9
Ross Crawford-d'Heureuse <r.crawford@douglas.de>
2017-04-25 13:41:39 +0200

    minor refinement

--- wobbuild/builder/services.py
+++ wobbuild/builder/services.py
@@ -150,35 +150,35 @@ class BuilderService(object):
     def before_steps(self, pipeline):
         for step in pipeline.get('before_steps', []):
             self.logger.info('perform before_step', {'step': step})
-            yield self.perform_step(path=self.the_build_path, step=step), step
+            yield self.perform_step(step=step), step
 
     def build_steps(self, pipeline):
         build = pipeline.get('build', {})
         if build.get('do') is True:
             for step in build.get('steps', []):
                 self.logger.info('perform build.step', {'step': step})
-                yield self.perform_step(path=self.the_build_path, step=step), step
+                yield self.perform_step(step=step), step
 
     def publish_steps(self, pipeline):
         publish = pipeline.get('publish', {})
         if publish.get('do') is True:
             for step in publish.get('steps', []):
                 self.logger.info('perform publish.step', {'step': step})
-                yield self.perform_step(path=self.the_build_path, step=step), step
+                yield self.perform_step(step=step), step
 
     def deploy_steps(self, pipeline):
         deploy = pipeline.get('deploy', {})
         if deploy.get('do') is True:
             for step in deploy.get('steps', []):
                 self.logger.info('perform deploy.step', {'step': step})
-                yield self.perform_step(path=self.the_build_path, step=step), step
+                yield self.perform_step(step=step), step
 
     def final_steps(self, pipeline):
         for step in pipeline.get('final_steps', []):
             self.logger.info('perform final_steps', {'step': step})
-            yield self.perform_step(path=self.the_build_path, step=step), step
+            yield self.perform_step(step=step), step
 
-    def perform_step(self, path, step):
+    def perform_step(self, step):
         if self.has_failed is True:
             return Failedresult()
 
@@ -196,15 +196,20 @@ class BuilderService(object):
             step = Template(step)
             step = step.render(**env_variables)
 
+            # set the env vars in the shell
             with shell_env(*env_variables):
-                with lcd(path):
+                # cd into the build path
+                with lcd(self.the_build_path):
+                    # execute the now templaterized step
                     return local(step, capture=True)
 
     def log_step(self, step_type, result, return_code, is_successful, step, took):
         if self.has_failed is None:
             self.has_failed = True if is_successful is False else None
 
+        # if we have a new build type that has no handler set it to system
         step_type = 'system' if step_type not in self.BUILD_LOG.keys() else step_type
+
         event = {
             'step_type': step_type,
             'step': step,
@@ -213,9 +218,12 @@ class BuilderService(object):
             'return_code': return_code,
             'took': took,
         }
+        # append to build
         self.BUILD_LOG[step_type].append(event)
+        # update the builds logs
         self.build.step_logs = self.BUILD_LOG
 
+        # oh we failed.. set the status
         if is_successful is False:
             self.build.status = 'failure' 
 
